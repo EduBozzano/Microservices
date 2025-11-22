689959a2f40192b1affb206b259156b89c3698d3
EduBozzano <bozzanoedu@gmail.com>
2025-11-22 16:10:36 -0300

    finalizacion del index

--- auth_service/src/index.js
+++ auth_service/src/index.js
@@ -18,5 +18,53 @@ const { logInfo, logError } = require('./utils/logger'); //utilizamos destructur
 const PORT = Number(ProcessingInstruction.env.PORT) || 3000; //el puerto del env devuelve un string entonces lo parseamos a un numero
 
 async function startServer(){
-    
+    try {
+        //cada logInfo seria un log de informacion que estariamos guardando
+        logInfo('bootstrapping: iniciando la conexion a la base de datos');
+        //nos conectamos a la base de datos
+        await connectToDatabase(Process.env.MONGO_URI);
+
+        logInfo('Inicializando la aplicacion Express...');
+        const app = createApp(); // creamos la app ya configurada (middlewares, rutas, manejador de errores.)
+
+        //cuando la db este lista arrancamos el servidor
+        const server = app.listen(PORT, () => {logInfo('Auth Service escuchando en puerto ${PORT}')});
+
+        //manejo graceful del shutdown
+        const gracefulShutdown = async  (signal) => {
+            try {
+                logInfo('Señal recibida ${signal}: cerrando servidor...')
+                //cerramos el servidor (detiene nuevas conexines, y espera que se completen las que ya estan)
+                server.close(() => logInfo('Servidor Cerrado.'));
+
+                //exportamos el db close y lo usamos para cerrar la conexion a la db
+                await requiere('./database').close();
+                logInfo('Conexcion a DB cerrada');
+                process.exit(0);
+            } catch (error) {
+                logError('Error durante gracefulShutdown', error);
+                process.exit(1);
+            }
+        };
+
+        //llamamos al shutdown dependiendo de la senal recibida
+        process.on('SIGINT', () => gracefulShutdown('SIGINT'));
+        process.on('SIGTERM', ()=> gracefulShutdown('SIGTERM'));
+
+        //capturamos errores no manejados //reason lo genera automaticamente node
+        process.on('unhandledRejection', (reason) => {logError('Unhandled Rejection - motivo:', reason)})
+
+        process.on('uncaughtException', (error) => {
+            logError('UncaughtException - error critico:', error);
+            process.exit(1) //(en produccion se suele reiniciar)
+        });
+
+    } catch (error) {
+        //si falla el bootstraping logueamos y salimos
+        logError('Error inicializando el servicio', error);
+        process.exit(1);
+    }
 }
+
+//inicializamos el proceso de arranque
+startServer();
